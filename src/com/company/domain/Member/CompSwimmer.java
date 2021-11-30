08d449d331e47993fb4677c50f5f3d547aeaa740
chri75ky <chri75ky@stud.kea.dk>
2021-11-30 14:17:23 +0100

    Tilføjet attributen coach til Compswimmer. Skal tilføje en metode som kan sætte en træner på ud fra svømmerens alder

--- src/com/company/domain/Member/CompSwimmer.java
+++ src/com/company/domain/Member/CompSwimmer.java
@@ -1,13 +1,15 @@
 package com.company.domain.Member;
 
 import com.company.domain.Member.Member;
+import com.company.domain.User;
 
 public class CompSwimmer extends Member {
     private String discipline;
-
+    private User coach;
     public CompSwimmer(String fullName, int age, boolean activeMembership, String discipline) {
         super(fullName, age, activeMembership);
         this.discipline = discipline;
+        //TODO Tilføj en måde at sætte en træner på svømmeren ud fra deres alder (under 18 vs 18 og over)
     }
 
     @Override
